72e6b135f61f897e05f45895f0a92bff4f14c204
Remi Azevedo <remi.azevedo01@gmail.com>
2025-04-13 20:56:03 -0300

    Feat: Adds champion data save script skeleton
    Introduces a script with TODOs to guide the creation of a champion data saving process.
    
    The script outlines the necessary steps for creating modules, API endpoints, and a saving mechanism for champion data, mirroring a champions.json structure.

---
+++ src/shared/saveChampionsData.ts
@@ -0,0 +1,9 @@
+//TODO: Criar o model dos champions na pasta models
+//TODO: Criar o módulo dos champions na pasta modules
+//TODO: Criar o endpoint de buscar todos os campeões
+//TODO: Criar o endpoint de buscar um campeão pelo id
+//TODO: Criar o endpoint de buscar um campeão pelo nome
+//TODO: Criar o endpoint de salvar um campeão
+//TODO: Criar aqui o script para salvar todos os campeões que estão no champions.json, utilizando o model e repository criados dentro da API
+//TODO; Criar uma branch para desenvolver essa tarefa e em seguida criar um pull request para a branch main
+// replicar toda a estrutura do arquivo champions.json porém removendo os campos que não são necessários ou são paths de assets que não temos
