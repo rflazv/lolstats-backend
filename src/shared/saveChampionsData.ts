6faec242345c52dc6c0940048f008df56d51d4ab
Remi Azevedo <remi.azevedo01@gmail.com>
2025-04-13 21:19:34 -0300

    Feat: Updates TODO comment for PR review process
    Clarifies the TODO comment regarding the pull request process,
    emphasizing the requirement for review by another person before
    merging into the main branch.

--- src/shared/saveChampionsData.ts
+++ src/shared/saveChampionsData.ts
@@ -5,5 +5,5 @@
 //TODO: Criar o endpoint de buscar um campeão pelo nome
 //TODO: Criar o endpoint de salvar um campeão
 //TODO: Criar aqui o script para salvar todos os campeões que estão no champions.json, utilizando o model e repository criados dentro da API
-//TODO; Criar uma branch para desenvolver essa tarefa e em seguida criar um pull request para a branch main
+//TODO; Criar uma branch para desenvolver essa tarefa e em seguida criar um pull request para a branch main, PR que deve ser revisado por outra pessoa antes de ser mergeado na branch main
 // replicar toda a estrutura do arquivo champions.json porém removendo os campos que não são necessários ou são paths de assets que não temos
